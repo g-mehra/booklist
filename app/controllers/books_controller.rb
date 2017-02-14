267d58c4485843a4b237e98c2bc15831238b16ea
g-mehra <gmehra@alumni.uwo.ca>
2017-02-13 19:02:04 -0500

    Complete assignment

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,19 @@
 class BooksController < ApplicationController
 
 
+def index
+  @books = Book.all
+  respond_to do |format|
+    format.html
+    format.text
+    format.csv do |books|
+      books = Book.generate_csv(@books)
+      render :plain => books
+    end
+    format.json do |books|
+      render :json => @books
+    end 
+  end
+end
+
 end
